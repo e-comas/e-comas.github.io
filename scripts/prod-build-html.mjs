fba0309ba49a36daecefe1ef4b483be7d14a394e
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-09-16 11:14:23 +0200

    build: optimizer images for most common viewport sizes

--- scripts/prod-build-html.mjs
+++ scripts/prod-build-html.mjs
@@ -24,14 +24,82 @@ export async function buildHTML() {
 
 const viewportsToTest = [
   {
+    width: 360,
+    height: 640,
+    deviceScaleFactor: 1,
+  },
+  {
+    // iPhone 8
+    width: 375,
+    height: 667,
+    deviceScaleFactor: 2,
+  },
+  {
+    // iPhone 8+
+    width: 414,
+    height: 736,
+    deviceScaleFactor: 3,
+  },
+  {
+    // iPhone X/12
+    width: 375,
+    height: 812,
+    deviceScaleFactor: 3,
+  },
+  {
+    // iPhone 11
+    width: 414,
+    height: 896,
+    deviceScaleFactor: 2,
+  },
+  {
+    // iPad
+    width: 768,
+    height: 1024,
+    deviceScaleFactor: 2,
+  },
+  {
+    // iPad
+    width: 1024,
+    height: 768,
+    deviceScaleFactor: 2,
+  },
+  {
+    // iPad Pro
+    width: 1024,
+    height: 1366,
+    deviceScaleFactor: 2,
+  },
+  {
+    // iPad Pro
+    width: 1366,
+    height: 1024,
+    deviceScaleFactor: 2,
+  },
+  {
     width: 640,
     height: 480,
+    deviceScaleFactor: 1,
+  },
+  {
+    width: 1440,
+    height: 900,
+    deviceScaleFactor: 1,
+  },
+  {
+    width: 1366,
+    height: 768,
     deviceScaleFactor: 1,
   },
   {
     width: 1920,
     height: 1080,
     deviceScaleFactor: 1,
+  },
+  {
+    width: 1920,
+    height: 1080,
+    deviceScaleFactor: 2,
   },
 ];
 
