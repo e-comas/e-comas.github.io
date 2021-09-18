a0242785e1676ee8ab600af30b7f8d711b4741dc
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-09-18 02:12:49 +0200

    build: minify CSS

--- scripts/prod-build-css.mjs
+++ scripts/prod-build-css.mjs
@@ -1,12 +1,21 @@
 import { writeFile } from "node:fs/promises";
+
+import cssnano from "cssnano";
+import postcss from "postcss";
 import sass from "sass";
+
 import { OUTPUT_DIR } from "./prod-config.mjs";
 import createHash from "./prod-hash.mjs";
 
 export default async function sass2css(sassFiles) {
-  const { css } = sass.renderSync({
+  const sassResult = sass.renderSync({
     data: sassFiles.map((path) => `@use ${JSON.stringify(path)}`).join(";"),
   });
+
+  const { css } = await postcss([cssnano({ preset: ["advanced"] })]).process(
+    sassResult.css,
+    { from: undefined, map: { annotation: false } }
+  );
 
   const hash = createHash(css);
   const fileName = `${hash}.css`;
