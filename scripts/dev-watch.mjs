93e9ce658a1ec39f4febd257291e36231bf9d56e
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-09-18 17:02:23 +0200

    dev: simplify watch script

--- scripts/dev-watch.mjs
+++ scripts/dev-watch.mjs
@@ -1,38 +1,21 @@
-import { watch, promises as fs } from "fs";
+import { watch } from "node:fs/promises";
 
 import { INPUT_DIR } from "./dev-config.mjs";
 import { sendRebuildSignal } from "./dev-build-js-from-worker.mjs";
-import { refreshBrowser } from "./dev-server.mjs";
-import { startServer } from "./dev-server.mjs";
+import { refreshBrowser, startServer } from "./dev-server.mjs";
 
+startServer();
+
+const watcher = watch(INPUT_DIR, { recursive: true });
 let antiRebound;
-const watcher = (event, fileName) => {
-  console.log(event, fileName);
-  if (antiRebound !== fileName) {
-    sendRebuildSignal(fileName);
-    antiRebound = fileName;
+for await (const { eventType, filename } of watcher) {
+  console.log(eventType, filename);
+  if (antiRebound !== filename) {
+    sendRebuildSignal(filename);
+    antiRebound = filename;
     setTimeout(() => {
-      if (antiRebound === fileName) antiRebound = null;
+      if (antiRebound === filename) antiRebound = null;
     }, 5000);
     refreshBrowser();
   }
-};
-
-const watchFile = (path) => watch(path, watcher);
-const watchDir = async (dir) => {
-  const promises = [];
-  const dh = await fs.opendir(dir);
-  for await (const dirent of dh) {
-    if (dirent.isDirectory()) {
-      promises.push(new URL(`./${dirent.name}/`, dir));
-    } else {
-      watchFile(new URL("./" + dirent.name, dir));
-    }
-  }
-
-  return Promise.all(promises.map(watchDir));
-};
-
-watchDir(INPUT_DIR)
-  .then(() => startServer())
-  .catch(console.error);
+}
