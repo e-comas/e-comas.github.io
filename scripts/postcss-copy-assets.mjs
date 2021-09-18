8bc0baf32be7d273f140a744497be6bdbe61dc22
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-09-18 12:45:18 +0200

    build: copy CSS assets

---
+++ scripts/postcss-copy-assets.mjs
@@ -0,0 +1,57 @@
+/* Inspired from https://github.com/shutterstock/postcss-copy-assets. */
+
+import fs from "node:fs";
+
+const cache = new WeakSet();
+export default function copyAssets({ base, urlTransform }) {
+  return {
+    postcssPlugin: "postcss-copy-assets",
+    Declaration(decl) {
+      if (!cache.has(decl) && decl.value?.includes("url(")) {
+        decl.value = decl.value.replace(/url\((.*?)\)/g, (_, urlMatch) => {
+          // Example:
+          //   decl.value = 'background: url("../../images/foo.png?a=123");'
+          //   urlMatch         = '"../../images/foo.png?a=123"'
+          //   copyOpts.base    = 'dist/assets'
+          //   postCssOpts.from = 'src/css/page/home.css'
+          //   postCssOpts.to   = 'dist/assets/css/home.min.css'
+
+          // "../../images/foo.png?a=123" -> ../../images/foo.png?a=123
+          if (/^\s*["']/.test(urlMatch))
+            urlMatch = urlMatch.trim().slice(1, -1);
+          const url = new URL(
+            new URL(urlMatch, "root://").toString().replace("root://", "."),
+            base
+          );
+
+          if (url.origin !== base.origin) return _;
+
+          let contents;
+          try {
+            // Read the original file
+            contents = fs.readFileSync(url);
+          } catch (err) {
+            console.warn("Cannot read asset", urlMatch, err.message);
+            return _;
+          }
+
+          const newAssetFileURL = urlTransform(url, contents);
+
+          const newUrlString = `url(${JSON.stringify(
+            newAssetFileURL.pathname.slice(base.pathname.length + 1) +
+              url.search +
+              url.hash
+          )})`;
+
+          fs.mkdirSync(new URL(".", newAssetFileURL), { recursive: true });
+          fs.writeFileSync(newAssetFileURL, contents);
+
+          cache.add(decl);
+
+          // Return the new url() string
+          return newUrlString;
+        });
+      }
+    },
+  };
+}
--- scripts/prod-build-css.mjs
+++ scripts/prod-build-css.mjs
@@ -4,21 +4,38 @@ import cssnano from "cssnano";
 import postcss from "postcss";
 import sass from "sass";
 
-import { OUTPUT_DIR } from "./prod-config.mjs";
+import copyAssets from "./postcss-copy-assets.mjs";
+
+import { INPUT_DIR, OUTPUT_DIR } from "./prod-config.mjs";
 import createHash from "./prod-hash.mjs";
 
+const cache = new WeakMap();
 export default async function sass2css(sassFiles) {
+  const cachedValue = cache.get(sassFiles);
+  if (cachedValue != null) return cachedValue;
+
   const sassResult = sass.renderSync({
     data: sassFiles.map((path) => `@use ${JSON.stringify(path)}`).join(";"),
   });
 
-  const { css } = await postcss([cssnano({ preset: ["advanced"] })]).process(
-    sassResult.css,
-    { from: undefined, map: { annotation: false } }
-  );
+  const { css } = await postcss([
+    cssnano({ preset: ["advanced"] }),
+    copyAssets({
+      base: INPUT_DIR,
+      urlTransform(url, contents) {
+        const lastDotIndex = url.pathname.lastIndexOf(".");
+        const ext = lastDotIndex === -1 ? "" : url.pathname.slice(lastDotIndex);
+        const fileName = createHash(contents) + ext;
+        console.log("writing to disk", fileName);
+        return new URL(fileName, OUTPUT_DIR);
+      },
+    }),
+  ]).process(sassResult.css, { from: undefined, map: { annotation: false } });
 
   const hash = createHash(css);
   const fileName = `${hash}.css`;
   await writeFile(new URL(fileName, OUTPUT_DIR), css);
+  console.log("written to disk", fileName);
+  cache.set(sassFiles, fileName);
   return fileName;
 }
