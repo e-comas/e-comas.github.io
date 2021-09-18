239cc22aab6c1dfcc7d46928e6622b0f3bf68342
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-09-18 18:05:46 +0200

    strategy: create page

---
+++ src/strategy.tsx
@@ -0,0 +1,71 @@
+import { h } from "@aduh95/async-jsx";
+
+import Body from "./views/Body.js";
+import Header from "./views/Header.js";
+import Footer from "./views/Footer.js";
+import Picture from "./utils/Picture.js";
+
+import "./strategy.scss";
+
+export default (
+  <Body>
+    <Header />
+    <main>
+      <section>
+        <h1>Strategy</h1>
+        <img aria-hidden="true" alt="" src="/images/chess.svg" />
+        <ul>
+          <li>Consumer Prices volatility</li>
+          <li>Lost Buy Box rates (prices lower from other selling parties) </li>
+          <li>
+            Insights and sentiments analysis on customer reviews and questions.
+          </li>
+        </ul>
+        
+        <p>
+          To check how your distribution is working and how the customer
+          perceive your brand and products.
+        </p>
+      </section>
+
+      <section class="twoThird">
+        <Picture src="/images/image00002.jpeg" alt="team picture" />
+        <Picture src="/images/IMG_5892.jpg" alt="team picture" />
+      </section>
+
+      <section class="howCanWeHelp">
+        <h2>How can we help?</h2>
+        <a class="cta">More about us</a>
+        <details>
+          <summary>Group coaching</summary>
+          <p>
+            With the Amazon Launch Accelerator program you will have access to,
+            and benefit from:
+          </p>
+          <ul>
+            <li>Strategic group calls;</li>
+            <li>A group page to answer any questions you may have;</li>
+            <li>
+              8 hours of video training which includes homework and tools;
+            </li>
+            <li>Discounts on tools and software from our partners;</li>
+          </ul>
+          <p>
+            The ONLY prerequisite is that you need to have an Amazon Account,
+            and the time to implement what we teach you.
+          </p>
+        </details>
+        <details>
+          <summary>Coaching</summary>
+        </details>
+        <details>
+          <summary>Aim of the workshop</summary>
+        </details>
+        <details>
+          <summary>Topics</summary>
+        </details>
+      </section>
+    </main>
+    <Footer />
+  </Body>
+).then((e) => document.body.append(e));
