ae2e3b65614040bb6aa9acab713ca6078de56100
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-09-19 15:57:55 +0200

    Add missing pages

---
+++ src/experience.tsx
@@ -0,0 +1,17 @@
+import { h } from "@aduh95/async-jsx";
+
+import Body from "./views/Body.js";
+import Header from "./views/Header.js";
+import Footer from "./views/Footer.js";
+
+import "./experience.scss";
+
+export default (
+  <Body>
+    <Header />
+    <main>
+      <p>TODO</p>
+    </main>
+    <Footer />
+  </Body>
+).then((e) => document.body.append(e));
---
+++ src/news.tsx
@@ -0,0 +1,17 @@
+import { h } from "@aduh95/async-jsx";
+
+import Body from "./views/Body.js";
+import Header from "./views/Header.js";
+import Footer from "./views/Footer.js";
+
+import "./contact.scss";
+
+export default (
+  <Body>
+    <Header />
+    <main>
+      <p>TODO</p>
+    </main>
+    <Footer />
+  </Body>
+).then((e) => document.body.append(e));
