dae84db3da3345460860dd4ab164d9f425f6e1b9
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-09-25 11:42:15 +0200

    Picture: add Safari workaround

--- src/utils/Picture.tsx
+++ src/utils/Picture.tsx
@@ -9,7 +9,11 @@ interface PictureProps {
 }
 
 export default (props: PictureProps) => (
-  <picture class={props.class || props.className}>
-    <img src={props.src} alt={props.alt} />
-  </picture>
+  // Using a div is necessary to workaround Safari interpretation of the spec
+  // https://stackoverflow.com/questions/44770074/css-grid-row-height-safari-bug
+  <div>
+    <picture class={props.class || props.className}>
+      <img src={props.src} alt={props.alt} />
+    </picture>
+  </div>
 );
