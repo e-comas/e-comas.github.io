5bec86f68f40cf9ed0a54f560020d56e6b2f5200
Sylvain <sylvain.tremblais99@gmail.com>
2021-09-30 18:37:13 +0200

    sylvain-ponctuation-list(2)

--- src/analytics.tsx
+++ src/analytics.tsx
@@ -10,53 +10,54 @@ export default (
     <Header />
     <Breadcrumbs />
     <main>
-      <p>OUR PARTNER: SHOPIFY</p>
-      <p>
-        We proudly partner with Shopify, the leading ecommerce platform allowing
-        brands to build and manage their own standalone online retail stores.{" "}
-      </p>
-      <p>
-        Shopify is the third largest online retailer behind Amazon and eBay in
-        the US, with 20% market share.
-      </p>
-      <p>
-        We believe Shopify is one of the keys to a successful omni-channel
-        online retail strategy.
-      </p>
-      <p>
-        Running a Shopify store alongside your Amazon and other ecommerce
-        accounts can boost your sales and amplify your brand.
-      </p>
-      <p>Some facts about Shopify: </p>
-      <ul>
-        <li>
-          Shopify is the fastest growing online store builder in the world
-        </li>
-        <li>Shopify is the most searched ecommerce solution </li>
-        <li>
-          In 2019, 62 million buyers worldwide made repeat purchases on Shopify
-          stores
-        </li>
-        <li>
-          From 2019 to 2020, Shopify stores saw an increase of 40% in customers
-        </li>
-        <li>
-          Shopify buyers purchased 3.8 times on average from the same store.
-        </li>
-      </ul>
-      <p>
-        At e-Comas, we can build you a strategy based on a Shopify store coupled
-        with Amazon and other ecommerce platforms.{" "}
-      </p>
-      <p>
-        <strong>
-          <em>
-            We can help you create and manage your Shopify store, drive growth
-            to it, boost your sales and grow your brand quickly, efficiently and
-            globally.
-          </em>
-        </strong>
-      </p>
+      <section>
+        <h2>Our partner: Shopify</h2>
+
+        <p>
+          We proudly partner with Shopify, the leading ecommerce platform allowing
+          brands to build and manage their own standalone online retail stores.{" "}
+        </p>
+
+        <p>
+          Shopify is the third largest online retailer behind Amazon and eBay in
+          the US, with 20% market share.
+        </p>
+
+        <p>
+          We believe Shopify is one of the keys to a successful omni-channel
+          online retail strategy.
+        </p>
+
+        <p>
+          Running a Shopify store alongside your Amazon and other ecommerce
+          accounts can boost your sales and amplify your brand.
+        </p>
+
+        <h5>Some facts about Shopify: </h5>
+
+        <ul>
+          <li>Shopify is the fastest growing online store builder in the world.</li>
+          <li>Shopify is the most searched ecommerce solution.</li>
+          <li>In 2019, 62 million buyers worldwide made repeat purchases on Shopify stores.</li>
+          <li>From 2019 to 2020, Shopify stores saw an increase of 40% in customers.</li>
+          <li>Shopify buyers purchased 3.8 times on average from the same store.</li>
+        </ul>
+
+        <p>
+          At e-Comas, we can build you a strategy based on a Shopify store coupled
+          with Amazon and other ecommerce platforms.{" "}
+        </p>
+
+        <p>
+          <strong>
+            <em>
+              We can help you create and manage your Shopify store, drive growth
+              to it, boost your sales and grow your brand quickly, efficiently and
+              globally.
+            </em>
+          </strong>
+        </p>
+      </section>
     </main>
     <Footer />
   </Body>
--- src/website-creation.tsx
+++ src/website-creation.tsx
@@ -10,63 +10,59 @@ export default (
     <Header />
     <Breadcrumbs />
     <main>
-      <h3 id="our-partner-profitero">OUR PARTNER: PROFITERO</h3>
-      <p>
-        We proudly partner with Profitero, the actionable ecommerce insights
-        platform, to help our customers harness the vast amount of data
-        available to them on Amazon, Shopify and other marketplaces.
-      </p>
-      <p>
-        Profitero’s insights combined with e-Comas’s expertise in advertising,
-        content and supply chain optimisation give you huge opportunities for
-        growth. You can stretch your limited team resources and achieve fast
-        ROI.
-      </p>
-      <p>Here’s an example of what they do: </p>
-      <ul>
-        <li>
-          Profitero’s client makes a product that competes with a famous brand
-        </li>
-        <li>The famous brand’s product is always #1 on Amazon</li>
-        <li>
-          The client uses Profitero’s assortment and availability module,
-          setting up alerts to monitor each time the famous brand’s product goes
-          out of stock
-        </li>
-        <li>
-          When that happens, they sponsor all branded keywords around the famous
-          product
-        </li>
-        <li>They increase their sales by 7%!</li>
-        <li>
-          The client switches off the ad spend as soon as the famous brand’s
-          product is back in stock.
-        </li>
-      </ul>
-      <p>That’s just one example!</p>
-      <p>
-        Profitero customers typically experience 70% ecommerce sales
-        acceleration over competitors.
-      </p>
-      <p>Profitero offers: </p>
-      <ul>
-        <li>Powerful, yet affordable insights</li>
-        <li>
-          Automation of research that normally takes a lot of your team’s time
-        </li>
-        <li>
-          Guidance, in partnership with e-Comas, to ensure the data is used.
-        </li>
-      </ul>
-      <p>
-        <strong>
-          <em>
-            We can help you action the valuable data you get from Profitero to
-            optimise your content, improve your conversion rates, save you time,
-            and improve the ROI on your ad spend.
-          </em>
-        </strong>
-      </p>
+      <section>
+        <h2>Our partner : Profitero</h2>
+
+        <p>
+          We proudly partner with Profitero, the actionable ecommerce insights
+          platform, to help our customers harness the vast amount of data
+          available to them on Amazon, Shopify and other marketplaces.
+        </p>
+        
+        <p>
+          Profitero’s insights combined with e-Comas’s expertise in advertising,
+          content and supply chain optimisation give you huge opportunities for
+          growth. You can stretch your limited team resources and achieve fast
+          ROI.
+        </p>
+
+        <h5>Here’s an example of what they do: </h5>
+
+        <ul>
+          <li>Profitero’s client makes a product that competes with a famous brand.</li>
+          <li>The famous brand’s product is always #1 on Amazon.</li>
+          <li>
+            The client uses Profitero’s assortment and availability module,
+            setting up alerts to monitor each time the famous brand’s product goes
+            out of stock.
+          </li>
+          <li>When that happens, they sponsor all branded keywords around the famous product.</li>
+          <li>They increase their sales by 7%!</li>
+          <li>The client switches off the ad spend as soon as the famous brand’s product is back in stock.</li>
+        </ul>
+
+        <p>That’s just one example!</p>
+
+        <p>Profitero customers typically experience 70% ecommerce sales acceleration over competitors.</p>
+
+        <h5>Profitero offers: </h5>
+        <ul>
+          <li>Powerful, yet affordable insights;</li>
+          <li>Automation of research that normally takes a lot of your team’s time;</li>
+          <li>Guidance, in partnership with e-Comas, to ensure the data is used.</li>
+        </ul>
+
+        <p>
+          <strong>
+            <em>
+              We can help you action the valuable data you get from Profitero to
+              optimise your content, improve your conversion rates, save you time,
+              and improve the ROI on your ad spend.
+            </em>
+          </strong>
+        </p>
+
+      </section>
     </main>
     <Footer />
   </Body>
