a912ef597ece33dfd9f024a5d4fd0d10fbb0571a
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-10-06 14:48:20 +0200

    our-team: add pictures

--- src/our-team.tsx
+++ src/our-team.tsx
@@ -1,8 +1,11 @@
 import { h } from "@aduh95/async-jsx";
 
+import team from "./utils/team.toml";
+
 import Body from "./views/Body.js";
 import Header from "./views/Header.js";
 import Breadcrumbs from "./views/Breadcrumbs.js";
+import Picture from "./utils/Picture.js";
 import Footer from "./views/Footer.js";
 
 import "./our-team.scss";
@@ -12,7 +15,22 @@ export default (
     <Header />
     <Breadcrumbs />
     <main>
-      <p>TODO</p>
+      <section>
+        <h2>Our team</h2>
+        <ul class="face-book">
+          {Object.values(team).map(({ Name, Title, pictureUrl }) => (
+            <li>
+              <figure>
+                <Picture src={pictureUrl} alt={"Picture of " + Name} />
+                <figcaption>
+                  <strong>{Name}</strong>
+                  <em>{Title}</em>
+                </figcaption>
+              </figure>
+            </li>
+          ))}
+        </ul>
+      </section>
     </main>
     <Footer />
   </Body>
