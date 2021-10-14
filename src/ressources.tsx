1d281e45db860f097775ef269c8840946b7af6d0
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-10-14 12:39:49 +0200

    ressources: add content

--- src/ressources.tsx
+++ src/ressources.tsx
@@ -5,6 +5,7 @@ import Header from "./views/Header.js";
 import Breadcrumbs from "./views/Breadcrumbs.js";
 import Footer from "./views/Footer.js";
 import Partners from "./views/Partners.js";
+import ElementWithIcon from "./utils/ElementWithIcon.js";
 import Picture from "./utils/Picture.js";
 
 import "./classic-page.scss";
@@ -14,11 +15,22 @@ export default (
     <Header />
     <Breadcrumbs />
     <main>
+      <ElementWithIcon tagName="section" src="/images/search.svg">
+        <h2>Ressources</h2>
+        <p>
+          We love to share our ecommerce expertise! There are many ways you can
+          learn from us.
+        </p>
+      </ElementWithIcon>
+
       <section>
         <figure>
           <figcaption>
             <h2>Read us</h2>
-            <p>Lorem ipsum</p>
+            <p>
+              We love to share our online retail expertise. Find out useful
+              ecommerce tips and what's new in the Amazon world.
+            </p>
             <a href="/news.html" class="cta">
               Check out our blog
             </a>
@@ -31,7 +43,11 @@ export default (
           <Picture src="/images/IMG_5892.jpg" alt="" />
           <figcaption>
             <h2>Learn from us</h2>
-            <p>Lorem ipsum</p>
+            <p>
+              We run our own popular webinar programme, talking to our friends
+              in the ecommerce world about how brands can improve and grow. See
+              our next events here!
+            </p>
             <a href="/webinar.html" class="cta">
               Check out our webinars
             </a>
@@ -42,7 +58,11 @@ export default (
         <figure>
           <figcaption>
             <h2>Listen to us</h2>
-            <p>Lorem ipsum</p>
+            <p>
+              Introducing the e-Comas podcast, now available on your favourite
+              streaming service! Listen now to find out how to grow on Amazon
+              and other ecommerce platforms.
+            </p>
             <a href="/webinar.html#podcast" class="cta">
               Check out our podcast
             </a>
@@ -55,9 +75,28 @@ export default (
           <Picture src="/images/IMG_5892.jpg" alt="" />
           <figcaption>
             <h2>White paper</h2>
-            <p>Lorem ipsum</p>
+            <p>
+              We research hot topics in the ecommerce industry and report our
+              findings in our in-depth white papers. Learn more from us here!
+            </p>
             <a href="/white-paper-form.html" class="cta">
               Check out our White Paper
+            </a>
+          </figcaption>
+        </figure>
+      </section>
+      <section>
+        <figure>
+          <Picture src="/images/IMG_5892.jpg" alt="" />
+          <figcaption>
+            <h2>Hear more from us</h2>
+            <p>
+              Sign up to our newsletter and we'll send you regular updates on
+              the world of Amazon, what's new in ecommerce, our upcoming
+              webinars and more!
+            </p>
+            <a href="/white-paper-form.html" class="cta">
+              Sign up for our newsletter
             </a>
           </figcaption>
         </figure>
