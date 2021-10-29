647706b8dc36391e089450930d4b6aef9391b1b2
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-10-29 23:17:08 +0200

    website_creation: update content

--- src/website-creation.tsx
+++ src/website-creation.tsx
@@ -7,7 +7,7 @@ import ElementWithIcon from "./utils/ElementWithIcon.js";
 import { ECommerce, EStore } from "./views/eWords.js";
 import Footer from "./views/Footer.js";
 
-import "./classic-page.scss";
+import "./shopify.scss";
 
 export default (
   <Body title="Website creation">
@@ -24,71 +24,91 @@ export default (
         commerce, the need for a selling branded website is critical for brands.
       </p>
 
-      <ElementWithIcon tagName="section" src="/images/search.svg">
-        <h2>Our partner: Shopify</h2>
+      <h3>
+        Native <ECommerce />
+      </h3>
 
-        <div>
-          <p>
-            We proudly partner with Shopify, the leading <ECommerce /> platform
-            allowing brands to build and manage their own standalone online
-            retail stores.
-          </p>
+      <p>
+        Each platform is working as a silo, keeping customers inside their
+        environment. It becomes of importance for brands to be able to speak the
+        language (in terms of technology, logistics and tactics) of these
+        platforms, but also to have it’s own <ECommerce /> website.
+      </p>
 
-          <p>
-            Shopify is the third largest online retailer behind Amazon and eBay
-            in the US, with 20% market share.
-          </p>
+      <section>
+        <h4>Why Shopify?</h4>
 
-          <p>
-            We believe Shopify is one of the keys to a successful omni-channel
-            online retail strategy.
-          </p>
+        <p>
+          We proudly partner with Shopify, the leading <ECommerce /> platform
+          allowing brands to build and manage their own standalone online retail
+          stores. Shopify is the third largest online retailer behind Amazon and
+          eBay in the US, with 20% market share.
+          <br />
+          We believe Shopify is one of the keys to a successful omni-channel
+          online retail strategy.
+          <br />
+          Running a Shopify store alongside your Amazon and other <ECommerce />{" "}
+          accounts can boost your sales and amplify your brand.
+        </p>
+        <p>Some facts about Shopify:</p>
+        <ul>
+          <li>
+            Shopify is the fastest growing online store builder in the world.
+          </li>
+          <li>
+            Shopify is the most searched <ECommerce /> solution.
+          </li>
+          <li>
+            In 2019, 62 million buyers worldwide made repeat purchases on
+            Shopify stores.
+          </li>
+          <li>
+            From 2019 to 2020, Shopify stores saw an increase of 40% in
+            customers.
+          </li>
+          <li>
+            Shopify buyers purchased 3.8 times on average from the same store.
+          </li>
+        </ul>
+      </section>
 
-          <p>
-            Running a Shopify store alongside your Amazon and other{" "}
-            <ECommerce />
-            accounts can boost your sales and amplify your brand.
-          </p>
-        </div>
-        <div>
-          <h3>Some facts about Shopify:</h3>
+      <section>
+        <h4>How we will help you</h4>
+        <p>We have a team of Shopify experts:</p>
+        <ul>
+          <li>UX designers;</li>
+          <li>Front end and back end designers;</li>
+          <li>
+            A <a href="syndication.html#content_creation">content team</a>;
+          </li>
+          <li>
+            <a href="consulting.html">
+              <ECommerce /> consultants;
+            </a>
+          </li>
+          <li>
+            An <a href="advertising.html">advertising team</a> to generate
+            traffic.
+          </li>
+        </ul>
+      </section>
 
-          <ul>
-            <li>
-              Shopify is the fastest growing online store builder in the world.
-            </li>
-            <li>
-              Shopify is the most searched <ECommerce /> solution.
-            </li>
-            <li>
-              In 2019, 62 million buyers worldwide made repeat purchases on
-              Shopify stores.
-            </li>
-            <li>
-              From 2019 to 2020, Shopify stores saw an increase of 40% in
-              customers.
-            </li>
-            <li>
-              Shopify buyers purchased 3.8 times on average from the same store.
-            </li>
-          </ul>
-
-          <p>
-            At e-Comas, we can build you a strategy based on a Shopify store
-            coupled with Amazon and other <ECommerce /> platforms.{" "}
-          </p>
-
-          <p>
-            <strong>
-              <em>
-                We can help you create and manage your Shopify store, drive
-                growth to it, boost your sales and grow your brand quickly,
-                efficiently and globally.
-              </em>
-            </strong>
-          </p>
-        </div>
-      </ElementWithIcon>
+      <section>
+        <h4>What we will do</h4>
+        <p>
+          Our strength is to support brands on their e-Commerce journey, on
+          marketplaces and on their own <ECommerce /> Shopify website.
+        </p>
+        <p>Our roadmap in three steps follows these steps:</p>
+        <p>Our process will start with:</p>
+        <ol>
+          <li>Au audit of your existing online presence.</li>
+          <li>Support to design your eCommerce strategy.</li>
+          <li>Briefing on your products and brand.</li>
+          <li>Development of your Shopify Site.</li>
+          <li>Daily Management of the site.</li>
+        </ol>
+      </section>
     </main>
     <Footer />
   </Body>
