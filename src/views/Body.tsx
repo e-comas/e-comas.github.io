d603ed156a703ec9d5f715d2c96823a1e682ffd2
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-10-29 11:15:04 +0200

    meta: fix TypeScript error/warning regarding class attribute in SVG

--- src/views/Body.tsx
+++ src/views/Body.tsx
@@ -29,6 +29,9 @@ declare module "react" {
   interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
     class?: string;
   }
+  interface SVGAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    class?: string;
+  }
 
   interface IframeHTMLAttributes<T> extends HTMLAttributes<T> {
     allowfullscreen?: boolean;
