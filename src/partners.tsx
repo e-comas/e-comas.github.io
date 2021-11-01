2567586947bdbe9ab2e4e05b9c80c56968b948a3
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-11-01 17:02:52 +0100

    Partners: add missing ones

--- src/partners.tsx
+++ src/partners.tsx
@@ -16,6 +16,91 @@ export default (
         <p>Lorem ipsum</p>
       </ElementWithIcon>
 
+      <section id="Reveals">
+        <h3>Reveals</h3>
+        <p>Lorem Ipsum dolor sit amet</p>
+        {/* <a href="reveals.html" class="cta">
+          Read further
+        </a> */}
+      </section>
+
+      <section id="Salsify">
+        <h3>Salsify</h3>
+        <p>
+          Salsify allows brands to manage product data, content and other rich
+          media assets to craft and deliver the consumer experience you want for
+          any channel you sell through. This empowers brands to deliver product
+          information that drives discovery, conversion and brand loyalty across
+          the digital shelf.
+        </p>
+        <a href="salsify.html" class="cta">
+          Read further
+        </a>
+      </section>
+
+      <section id="Shopify">
+        <h3>Shopify</h3>
+        <p>
+          We proudly partner with Shopify, the leading ecommerce platform
+          allowing brands to build and manage their own standalone online retail
+          stores.
+        </p>
+        <a href="website-creation.html#shopify" class="cta">
+          Read further
+        </a>
+      </section>
+
+      <section id="Profitero">
+        <h3>Profitero</h3>
+        <p>
+          Profitero provides over 4000 global brands with cutting edge
+          technology designed to accelerate e-commerce sales and save time.
+        </p>
+        <a href="profitero.html" class="cta">
+          Read further
+        </a>
+      </section>
+
+      <section id="Storfund">
+        <h3>Storfund</h3>
+
+        <p>
+          We partner with Storfund to offer our customers reliable daily cash
+          flow – with no delay between your products being bought, and you
+          getting paid.
+        </p>
+        <p>
+          Sell today, get paid today: There's no data submission, no payment
+          requests, just reliable daily cash flow.
+        </p>
+
+        <p>How it works:</p>
+        <ol>
+          <li>You sell as usual on your marketplace.</li>
+          <li>
+            Each day Storfund advances 80% of your net balance. (Your net
+            balance = gross sales minus marketplace fees and refunds)
+          </li>
+          <li>
+            When your marketplace pays out, you receive the remaining 20%.
+          </li>
+        </ol>
+
+        <p>Storfund charges between 1-2% flat fee, with no hidden fees.</p>
+        <p>
+          The fee is applied on the total net Amazon balance and not on gross
+          sales.
+        </p>
+        <p>
+          The service is currently available on 17 Amazon marketplaces and
+          CDiscount.
+        </p>
+
+        <a class="cta" href="https://www.storfund.com/r/Oq7fG1-SD">
+          Get on board
+        </a>
+      </section>
+
       <section id="GETIDA">
         <h3>GETIDA</h3>
         <p>
@@ -90,46 +175,6 @@ export default (
         </p>
         <a class="cta" href="https://register.payoneer.com/en/solution/ecomas/">
           Sign up and receive $25
-        </a>
-      </section>
-
-      <section id="Storfund">
-        <h3>Storfund</h3>
-
-        <p>
-          We partner with Storfund to offer our customers reliable daily cash
-          flow – with no delay between your products being bought, and you
-          getting paid.
-        </p>
-        <p>
-          Sell today, get paid today: There's no data submission, no payment
-          requests, just reliable daily cash flow.
-        </p>
-
-        <p>How it works:</p>
-        <ol>
-          <li>You sell as usual on your marketplace.</li>
-          <li>
-            Each day Storfund advances 80% of your net balance. (Your net
-            balance = gross sales minus marketplace fees and refunds)
-          </li>
-          <li>
-            When your marketplace pays out, you receive the remaining 20%.
-          </li>
-        </ol>
-
-        <p>Storfund charges between 1-2% flat fee, with no hidden fees.</p>
-        <p>
-          The fee is applied on the total net Amazon balance and not on gross
-          sales.
-        </p>
-        <p>
-          The service is currently available on 17 Amazon marketplaces and
-          CDiscount.
-        </p>
-
-        <a class="cta" href="https://www.storfund.com/r/Oq7fG1-SD">
-          Get on board
         </a>
       </section>
     </main>
