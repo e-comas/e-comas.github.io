0cf243c28f024370f5e16aebaf7a88fe8dafe7db
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-11-19 10:28:26 +0100

    HoverableMenu: fix Safari mobile hover

--- src/views/HoverableMenu.tsx
+++ src/views/HoverableMenu.tsx
@@ -2,4 +2,12 @@ import { h } from "@aduh95/async-jsx";
 
 import "./HoverableMenu.scss";
 
-export default (props: any) => <div class="hoverable-menu" />;
+export default (props: any, children: any[]) => (
+  <div class="hoverable-menu">
+    {children.map(async (child) => {
+      child = await child;
+      child.setAttribute("onclick", "");
+      return child;
+    })}
+  </div>
+);
