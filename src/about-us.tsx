0c6e3efb5a07843a2b4e7d68032b085a5455f44a
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-11-30 16:38:35 +0100

    about-us: add page

--- src/about-us.tsx
+++ src/about-us.tsx
@@ -3,152 +3,53 @@ import { h } from "@aduh95/async-jsx";
 import Body from "./views/Body.js";
 import Header from "./views/Header.js";
 import Breadcrumbs from "./views/Breadcrumbs.js";
-import Testimonies from "./views/Testimonies.js";
 import Picture from "./utils/Picture.js";
 import { EComas, ECommerce } from "./views/eWords.js";
-import ElementWithIcon from "./utils/ElementWithIcon.js";
 import Footer from "./views/Footer.js";
 
-import "./classic-page.scss";
+import "./about-us.scss";
 
 export default (
   <Body title="About eComas">
     <Header />
     <Breadcrumbs />
     <main>
-      <ElementWithIcon tagName="section" src="/images/trolley.svg">
-        <h2>About e-Comas</h2>
-        <p>
-          e-Comas - <ECommerce /> Made Simple was founded by <ECommerce />{" "}
-          expert Jérôme de Guigné in 2013, with the simple idea of helping
-          brands sell products online.
-        </p>
-        <p>
-          We quickly realised companies were struggling with establishing
-          profitable and sustainable <ECommerce /> strategies. That’s when
-          ‘Amazon Made Simple’ was born, which later became ‘<ECommerce /> Made
-          Simple’ - e-Comas.
-        </p>
+      <h2>How it started</h2>
+      <Picture
+        src="/images/jerome-thinking.png"
+        alt="Jérôme with a thinking face"
+      />
+      <p>
+        <EComas /> – <ECommerce /> Made Simple was founded by Jérôme de Guigné
+        in 2013, with the simple idea of helping brands sell products online. We
+        quickly realised companies were struggling with establishing profitable
+        and sustainable <ECommerce /> strategies. That’s when ‘Amazon Made
+        Simple’ was born, which later became ‘<ECommerce /> Made Simple’ –
+        <EComas />.
+      </p>
 
-        <p>
-          Today we work with major multinational brands to define and design
-          strong omnichannel expansion strategies, integrating Amazon, Shopify,
-          CDiscount, Veepee, Bol, Walmart and all other major <ECommerce />
-          platforms.
-          <br />
-          With a hands-on approach, our experts coach brands and handle their
-          accounts to sustainably grow their business and improve their agility
-          in tackling everyday challenges with online retail. We get you set up,
-          we scale you and make the business profitable, handling your account,
-          the content and the advertising.
-        </p>
-        <p>
-          Put simply, we help brands grow faster on <ECommerce /> - globally,
-          fast, across multiple online channels!
-        </p>
-        {/* <p style={"display:flex;justify-content:center" as any}> */}
-        <p>
-          <br />
-          <a href="/our-team.html" class="cta">
-            The <EComas /> team
-          </a>
-        </p>
-      </ElementWithIcon>
-      <section>
-        <figure>
-          <figcaption>
-            <h2>Experience</h2>
-
-            <p>
-              We have many years of{" "}
-              <strong>
-                <ECommerce /> experience
-              </strong>{" "}
-              between our amazing 45-strong team, and it comes from{" "}
-              <strong>all the right places</strong> – Amazon and other
-              platforms, brand owners and trade specialists.
-            </p>
-
-            <p>
-              We've helped many brands, big and small, grow their online
-              presence and <strong>increase their sales</strong> through Amazon
-              and other <ECommerce /> platforms. Don't just take our word for it
-              – see our <strong>stats</strong> and <strong>testimonials</strong>{" "}
-              from happy customers.
-            </p>
-          </figcaption>
-          <Picture src="/images/IMG_5892.jpg" alt="" />
-        </figure>
-      </section>
-
-      <section>
-        <figure>
-          <Picture src="/images/IMG_5892.jpg" alt="" />
-          <figcaption>
-            <h3>Figures</h3>
-            <h5>The numbers speak for themselves...</h5>
-
-            <ul>
-              <li>
-                We can increase sales by <strong>5 times</strong> within 2 years
-                … Sometimes up to <strong>10 times</strong> in 2 years.
-              </li>
-              <li>
-                We can reduce operational costs by <strong>50%</strong>.
-              </li>
-              <li>
-                We've managed <strong>+100,000</strong> listings.
-              </li>
-              <li>
-                We've opened <strong>+500</strong> Vendor/Seller accounts.
-              </li>
-              <li>
-                We've handled <strong>+€1,000,000</strong> in advertising
-                campaigns.
-              </li>
-            </ul>
-
-            <br />
-
-            <h5>Some more stats</h5>
-
-            <ul>
-              <li>
-                <ECommerce /> retail sales are forecast to grow to{" "}
-                <strong>$3.4 trillion</strong> by 2025 (Forbes).
-              </li>
-              <li>
-                US retail m-commerce sales grew <strong>41.4%</strong> in 2020
-                and will grow another <strong>15.2%</strong> in 2021, to reach{" "}
-                <strong>$359.32 billion</strong>. Annual sales should nearly{" "}
-                <strong>double</strong> between now and 2025 (
-                <a href="http://www.emarketer.com" target="_blank">
-                  www.emarketer.com
-                </a>
-                ).
-              </li>
-              <li>
-                By 2023, there will be <strong>5.3 billion</strong> total
-                internet users - <strong>66%</strong> of the global population (
-                <a href="http://www.cisco.com" target="_blank">
-                  www.cisco.com
-                </a>
-                ).
-              </li>
-              <li>
-                Consumer attention span is a maximum of{" "}
-                <strong>8 seconds!</strong> (
-                <a href="https://ebusinessinstitute.com/" target="_blank">
-                  www.ebusinessinstitute.com
-                </a>
-                ).
-              </li>
-            </ul>
-          </figcaption>
-        </figure>
-      </section>
-
-      <Testimonies withBlock />
+      <h2>How it's going</h2>
+      <Picture
+        src="/images/team-picture.png"
+        alt="e-Comas Europe team picture"
+      />
+      <p>
+        Today our experts coach brands and handle their accounts to sustainably
+        grow their business and improve their agility in tackling everyday
+        challenges with online retail. Working across Amazon, Shopify,
+        CDiscount, Veepee, Bol, Walmart and more, we get you set up, we scale
+        your business and make it profitable, handling your account, content and
+        advertising.
+        <br />
+        Put simply, we help brands grow faster on <ECommerce /> – globally,
+        quickly, across multiple online channels!
+      </p>
+      <a href="/our-team.html" className="cta">
+        Meet our team
+      </a>
+      <a href="/testimonials.html" className="cta">
+        Read our clients' testimonials
+      </a>
     </main>
     <Footer />
   </Body>
