ec487dc334026cc285de325cbcfd92db3466e4a2
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-12-06 10:05:20 +0100

    fix ts error

--- src/views/video-embed-runtime.ts
+++ src/views/video-embed-runtime.ts
@@ -1,7 +1,7 @@
 import { clickHandler } from "../utils/yt-embeder.js";
 
 for (const embed of document.querySelectorAll(".video-link>a[data-embed]")) {
-  embed.addEventListener("click", clickHandler, { once: true });
+  embed.addEventListener("click", clickHandler as any, { once: true });
 }
 
 export {};
