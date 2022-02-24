25b7e6030cef36207718868a5920a6c144322316
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-02-24 16:37:59 +0100

    build: copy admin folder from `main`

--- scripts/prod-build.mjs
+++ scripts/prod-build.mjs
@@ -6,30 +6,36 @@ import { OUTPUT_DIR } from "./prod-config.mjs";
 
 import findPages from "./generate-web-tree.mjs";
 import { crawlPages } from "./prod-build-html.mjs";
+import copyStaticFiles from "./prod-copy-static-files.mjs";
 
 process.env.NODE_ENV = "production";
 
 const closeServer = await startServer();
 
-let browser;
-try {
-  browser = await puppeteer.launch(process.env.DEBUG && { devtools: true });
-  const pages = await crawlPages(findPages(browser));
-  if (process.env.DEBUG) await new Promise(Function.prototype);
-  for (const page of pages) {
-    const url = new URL(page.url());
-    await writeFile(
-      new URL(
-        url.pathname === "/" ? "./index.html" : "." + url.pathname,
-        OUTPUT_DIR
-      ),
-      await page.content()
-    );
-    console.log("Wrote", url.pathname);
-  }
-} finally {
-  await browser?.close();
-}
+await Promise.all([
+  (async () => {
+    let browser;
+    try {
+      browser = await puppeteer.launch(process.env.DEBUG && { devtools: true });
+      const pages = await crawlPages(findPages(browser));
+      if (process.env.DEBUG) await new Promise(Function.prototype);
+      for (const page of pages) {
+        const url = new URL(page.url());
+        await writeFile(
+          new URL(
+            url.pathname === "/" ? "./index.html" : "." + url.pathname,
+            OUTPUT_DIR
+          ),
+          await page.content()
+        );
+        console.log("Wrote", url.pathname);
+      }
+    } finally {
+      await browser?.close();
+    }
+  })(),
+  copyStaticFiles(),
+]);
 
 console.log("Closing server");
 await closeServer();
---
+++ scripts/prod-copy-static-files.mjs
@@ -0,0 +1,14 @@
+import { cp } from "fs/promises";
+
+import { INPUT_DIR, OUTPUT_DIR } from "./prod-config.mjs";
+
+export default async function copyStaticFiles() {
+  const staticDirs = ["./admin"];
+
+  for (const dirName of staticDirs) {
+    await cp(new URL(dirName, INPUT_DIR), new URL(dirName, OUTPUT_DIR), {
+      recursive: true,
+      force: true,
+    });
+  }
+}
