4529bd278a0ca0e5554667697d7b61d72c707ad2
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-03-16 00:39:16 +0100

    webinar: add last webinar post

--- src/webinar.tsx
+++ src/webinar.tsx
@@ -1,5 +1,7 @@
 import { h } from "@aduh95/async-jsx";
 
+import yaml from "./utils/yaml.js";
+
 import Body from "./views/Body.js";
 import Header from "./views/Header.js";
 import Breadcrumbs from "./views/Breadcrumbs.js";
@@ -18,6 +20,36 @@ export default (
     <Breadcrumbs />
     <main id="main-content">
       <h2>Webinars</h2>
+      {"{% if site.tags.Webinar.length != 0 %}"}
+      <article>
+        <a href="{{ site.tags.Webinar[0].url }}">
+          <img
+            src="{{ site.tags.Webinar[0].thumbnail }}"
+            alt="{{ site.tags.Webinar[0].thumbnail_alt }}"
+            loading="lazy"
+          />
+        </a>
+        <time
+          dateTime={
+            '{{ site.tags.Webinar[0].date | date: "%Y-%m-%dT%H:%M:%S" }}'
+          }
+        >
+          {'{{ site.tags.Webinar[0].date | date: "%b %d, %Y" }}'}
+        </time>
+        <h3>
+          <a href="{{ site.tags.Webinar[0].url }}">
+            {"{{ site.tags.Webinar[0].title | escape }}"}
+          </a>
+        </h3>
+        <p>
+          <a href="{{ site.tags.Webinar[0].url }}">
+            {"{{ site.tags.Webinar[0].excerpt | escape }}"}
+          </a>
+        </p>
+        <a href="{{ site.tags.Webinar[0].url }}" class="cta">
+          Read the article
+        </a>
+      </article>
       <p>
         We run our own popular webinar programme, talking to our friends in the{" "}
         <ECommerce /> world about how brands can improve and grow. See our
@@ -40,4 +72,11 @@ export default (
     </main>
     <Footer />
   </Body>
-).then((e) => document.body.append(e));
+).then(
+  (e) => (
+    document.body.append(e),
+    yaml`
+layout: none
+`
+  )
+);
