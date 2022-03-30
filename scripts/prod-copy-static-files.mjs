ba48690e31a19331afa0bd8eb5bd09c974d0cecb
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-03-30 15:51:16 +0200

    fixup! build: enable Jekyll `future` build option
    This reverts commit 5cb78db8677997859fa924e6979ec890ff311707.

--- scripts/prod-copy-static-files.mjs
+++ scripts/prod-copy-static-files.mjs
@@ -3,10 +3,10 @@ import { cp } from "fs/promises";
 import { INPUT_DIR, OUTPUT_DIR } from "./prod-config.mjs";
 
 export default async function copyStaticFiles() {
-  const statics = ["./admin/", "./_config.yml"];
+  const staticDirs = ["./admin/"];
 
-  for (const name of statics) {
-    await cp(new URL(name, INPUT_DIR), new URL(name, OUTPUT_DIR), {
+  for (const dirName of staticDirs) {
+    await cp(new URL(dirName, INPUT_DIR), new URL(dirName, OUTPUT_DIR), {
       recursive: true,
       force: true,
     });
