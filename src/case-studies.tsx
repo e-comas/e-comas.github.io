0e8f07449055ee685b8c3f299c4720dbee302972
Antoine du Hamel <duhamelantoine1995@gmail.com>
2023-01-25 10:09:11 +0100

    case-studies: update images

--- src/case-studies.tsx
+++ src/case-studies.tsx
@@ -14,31 +14,31 @@ import "runtime:./utils/open-form-in-modal.js";
 
 const testimonies = [
   {
-    image: "/images/case-studies/michelin.jpg",
+    image: "/images/case-studies/michelin.png",
     name: "Saving time through upload automation",
     text: "When you have thousands of product listings, you need a robust solution for content management and upload. Here’s how we and our partners at Salsify helped a leading tyres brand save time and improve consistency.",
     link: "//go.e-comas.com/l/885733/2022-09-02/bwml5",
   },
   {
-    image: "/images/case-studies/zerowater.jpg",
+    image: "/images/case-studies/zerowater.png",
     name: "Finding the ad strategy that works",
     text: "There are lots of different ways to advertise Amazon, depending on what you want your ads to do. Find out how we helped ZeroWater find the winning formula for their advertising on Amazon DSP.",
     link: "//go.e-comas.com/l/885733/2023-01-16/l812r",
   },
   {
-    image: "/images/case-studies/suki.jpg",
+    image: "/images/case-studies/suki.png",
     name: "Building a strategy on insight",
     text: "Moving primarily B2B and traditional sales into the online retail sphere can be daunting. Here’s how we helped hardware market leader Suki design a strategy to take their successful products to Amazon.",
     link: "//go.e-comas.com/l/885733/2023-01-16/l812y",
   },
   {
-    image: "/images/case-studies/miamily.jpg",
+    image: "/images/case-studies/miamily.png",
     name: "The social strategy",
     text: "If you’re selling on Amazon, your ad strategy doesn’t need to be limited to Amazon. Here’s how we helped Swiss baby brand MiaMily interweave their social media and advertising to enhance their brand presence and grow their sales.",
     link: "//go.e-comas.com/l/885733/2023-01-16/l812v",
   },
   {
-    image: "/images/case-studies/mycarrypotty.jpg",
+    image: "/images/case-studies/mycarrypotty.png",
     name: "Creating content for other countries",
     text: "When expanding globally, the key to building trust in your brand is good-quality localised content. Here’s how we helped My Carry Potty grow their Amazon presence internationally.",
     link: "//go.e-comas.com/l/885733/2023-01-16/l813c",
