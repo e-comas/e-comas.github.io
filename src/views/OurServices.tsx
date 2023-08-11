3486687de060d090f88c57569345ab813c274ec1
Antoine du Hamel <duhamelantoine1995@gmail.com>
2023-08-11 18:33:00 +0200

    OurServices: add slide-in animation

--- src/views/OurServices.tsx
+++ src/views/OurServices.tsx
@@ -7,36 +7,52 @@ import GlobalExpansion from "./GlobalExpansion.svg";
 import RetailMedia from "./RetailMedia.svg";
 import Technology from "./Technology.svg";
 
+import "runtime:./animate-in.ts";
+
 export default () => (
   <section id="our-services">
     <h3>Our services</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quidem ad, autem officiis nobis eius quia sunt deserunt temporibus, nihil veritatis voluptatem perspiciatis? Dignissimos, magni. Doloremque vitae eveniet aliquam alias?</p>
-    <figure>
-        <img src="/images/consulting01.png" alt="illustration" />
-        <figcaption>
-            <GlobalExpansion />
-            <h4>Global Expansion</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
-            <a href="about:blank">Discover more</a>
-        </figcaption>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quidem
+      ad, autem officiis nobis eius quia sunt deserunt temporibus, nihil
+      veritatis voluptatem perspiciatis? Dignissimos, magni. Doloremque vitae
+      eveniet aliquam alias?
+    </p>
+    <figure class="animate-in">
+      <img src="/images/consulting01.png" alt="illustration" />
+      <figcaption>
+        <GlobalExpansion />
+        <h4>Global Expansion</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+          tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+        </p>
+        <a href="about:blank">Discover more</a>
+      </figcaption>
     </figure>
-    <figure>
-        <img src="/images/consulting02.png" alt="illustration" />
-        <figcaption>
-            <RetailMedia />
-            <h4>Retail Media</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
-            <a href="about:blank">Discover more</a>
-        </figcaption>
+    <figure class="animate-in">
+      <img src="/images/consulting02.png" alt="illustration" />
+      <figcaption>
+        <RetailMedia />
+        <h4>Retail Media</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+          tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+        </p>
+        <a href="about:blank">Discover more</a>
+      </figcaption>
     </figure>
-    <figure>
-        <img src="/images/consulting03.png" alt="illustration" />
-        <figcaption>
-            <Technology />
-            <h4>Technology</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
-            <a href="about:blank">Discover more</a>
-        </figcaption>
+    <figure class="animate-in">
+      <img src="/images/consulting03.png" alt="illustration" />
+      <figcaption>
+        <Technology />
+        <h4>Technology</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+          tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+        </p>
+        <a href="about:blank">Discover more</a>
+      </figcaption>
     </figure>
   </section>
 );
---
+++ src/views/animate-in.ts
@@ -0,0 +1,54 @@
+const cache = new WeakMap();
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    for (const entry of entries) {
+      if (entry.isIntersecting) {
+        requestAnimationFrame(cache.get(entry.target));
+      }
+    }
+  },
+  { threshold: 0.05 }
+);
+
+function activate() {
+  for (const elem of document.querySelectorAll(".animate-in")) {
+    const { style } = elem as HTMLElement;
+    style.setProperty("--animate-in-transform", "300%");
+    cache.set(elem, () => {
+      setTimeout(() => {
+        style.setProperty("--animate-in-transform", "0");
+      }, 100);
+    });
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        elem.classList.add("animate-in-ready");
+      });
+    });
+    observer.observe(elem);
+  }
+}
+
+function deactivate() {
+  for (const elem of document.querySelectorAll(".animate-in")) {
+    const { classList, style } = elem as HTMLElement;
+    classList.remove("animate-in-ready");
+    style.removeProperty("--animate-in-transform");
+  }
+  observer.disconnect();
+}
+
+const mediaQuery = matchMedia("(prefers-reduced-motion: reduce)");
+
+function toggle() {
+  if (mediaQuery.matches) {
+    deactivate();
+  } else {
+    activate();
+  }
+}
+
+mediaQuery.addEventListener("change", toggle);
+toggle();
+
+export {};
