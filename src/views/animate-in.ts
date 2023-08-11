c20d24ecede574f8bab82fddf57a41a273e3a364
Antoine du Hamel <duhamelantoine1995@gmail.com>
2023-08-11 19:28:10 +0200

    index: fix intro

--- src/views/animate-in.ts
+++ src/views/animate-in.ts
@@ -14,7 +14,7 @@ const observer = new IntersectionObserver(
 function activate() {
   for (const elem of document.querySelectorAll(".animate-in")) {
     const { style } = elem as HTMLElement;
-    style.setProperty("--animate-in-transform", "300%");
+    style.setProperty("--animate-in-transform", "100%");
     cache.set(elem, () => {
       setTimeout(() => {
         style.setProperty("--animate-in-transform", "0");
