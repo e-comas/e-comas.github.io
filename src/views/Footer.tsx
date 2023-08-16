41cf9ca97f2ec84da895266feb0ba3aa3920e0c9
Stéphane du Hamel <stephduh@live.fr>
2023-08-16 23:22:51 +0200

    footer: free consultation style

--- src/views/Footer.tsx
+++ src/views/Footer.tsx
@@ -29,6 +29,7 @@ export default () => (
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
       luctus nec ullamcorper mattis, pulvinar dapibus leo.
       <br />
+      <br />
       <FontAwesomeIcon icon={faEnvelopeOpen as IconProp} color="gold" />
       hello@ecomas.com
       </div>
