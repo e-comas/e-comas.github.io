16c290de03f3fca22e9e66cc43ee0f5a9c49113e
Stéphane du Hamel <stephduh@live.fr>
2023-08-19 23:23:40 +0200

    webinar: new layout

--- src/webinar.tsx
+++ src/webinar.tsx
@@ -12,33 +12,38 @@ import webinars_playlists from "./utils/webinars.toml";
 
 import "./views/VideoEmbedLink.scss";
 import "runtime:./utils/youtube_feed.ts";
-import "./resources.scss";
 import "./webinar.scss";
 
 export default (
   <Body title="Webinar">
     <Header />
+    <div class="hero">
+      <h1> Webinar</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel urna
+        eget erat lacinia pretium. Donec non imperdiet odio, ullamcorper gravida
+        nunc.
+      </p>
+      <a href="TODO" class="cta">
+        Register Now
+      </a>
+    </div>
     <Breadcrumbs />
     <main id="main-content">
-      <div class="hero">
-        <h1> Webinar</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel urna
-          eget erat lacinia pretium. Donec non imperdiet odio, ullamcorper
-          gravida nunc.
-        </p>
-        <a href="TODO" class ="cta"> Register Now</a>
-      </div>
-      <h2>Webinars</h2>
-      {"{% if site.tags.Webinar.length != 0 %}"}
+        {/* {"{% if site.tags.Webinar.length != 0 %}"}
       <article>
         <a href="{{ site.tags.Webinar[0].url }}">
           <img
-            src="{{ site.tags.Webinar[0].thumbnail }}"
+            src="images/chess.svg"
             alt="{{ site.tags.Webinar[0].thumbnail_alt }}"
             loading="lazy"
           />
         </a>
+        <h3>
+          <a href="{{ site.tags.Webinar[0].url }}">
+            Amazon Advertising Metrics: Should ACoS Be Your North Star Metric?
+          </a>
+        </h3>
         <time
           dateTime={
             "{{ site.tags.Webinar[0].date | date: '%Y-%m-%dT%H:%M:%S' }}"
@@ -46,40 +51,38 @@ export default (
         >
           {'{{ site.tags.Webinar[0].date | date: "%b %d, %Y" }}'}
         </time>
-        <h3>
-          <a href="{{ site.tags.Webinar[0].url }}">
-            {"{{ site.tags.Webinar[0].title | escape }}"}
-          </a>
-        </h3>
         <p>
-          <a href="{{ site.tags.Webinar[0].url }}">
-            {"{{ site.tags.Webinar[0].excerpt | escape }}"}
-          </a>
+          A holistic approach is crucial for measuring the effectiveness of your
+          Amazon advertising campaigns. Join us for our next webinar with Amazon
+          Ads as Nella Argenziano, Head of Advertising at e-Comas, explains why.
         </p>
         <a href="{{ site.tags.Webinar[0].url }}" class="cta">
           Register now
         </a>
       </article>
-      {"{% endif %}"}
-      <p>
-        We run our own popular webinar programme, talking to our friends in the{" "}
-        <ECommerce /> world about how brands can improve and grow. See our
-        previous events here!
-      </p>
+      {"{% endif %}"} */}
 
-      {webinars_playlists.map(({ title, playlist_id }) => (
-        <section class="webinars">
-          <h3>{title}</h3>
-          <a
-            href={`https://youtube.com/playlist?list=${playlist_id}`}
-            data-embed-playlist={playlist_id}
-            target="_blank"
-            rel="noopener"
-          >
-            Watch the videos on YouTube
-          </a>
-        </section>
-      ))}
+      {"{% for webinar in site.tags.Webinar | slice: 0, 3 %}"}
+      <article>
+        <a href="{{ webinar.url }}">
+          <img
+            src="{{ webinar.thumbnail }}"
+            alt="{{ webinar.thumbnail_alt }}"
+            loading="lazy"
+          />
+        </a>
+        <h3>
+          <a href="{{ webinar.url }}">{"{{ webinar.title | escape }}"}</a>
+        </h3>
+        <time dateTime={"{{ webinar.date | date: '%Y-%m-%dT%H:%M:%S' }}"}>
+          {'{{ webinar.date | date: "%b %d, %Y" }}'}
+        </time>
+        <p>{"{{ webinar.excerpt | escape }}"}</p>
+        <a href="{{ webinar.url }}" class="cta">
+          Register now
+        </a>
+      </article>
+      {"{% endfor %}"}
     </main>
     <Footer />
   </Body>
