8a6e3358b94bde933fb08787204138816413cba3
Stéphane du Hamel <stephduh@live.fr>
2023-09-08 11:03:49 +0200

    global-expansion: refactor

--- src/global-expansion.tsx
+++ src/global-expansion.tsx
@@ -11,13 +11,15 @@ import Hero from "./views/hero.tsx";
 
 import "./views/animate-in.scss";
 import "runtime:./views/animate-in.ts";
-import WhatWeDo from "./views/what-we-do.tsx";
 import Picture from "./utils/Picture.tsx";
 import FlipCard from "./views/flipCard.tsx";
 import { FontAwesomeIcon } from "@aduh95/jsx-fontawesome";
-import { faDownload, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faBullhorn, faDownload, faMagnifyingGlass, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { EComas, ECommerce } from "./views/eWords.tsx";
+import ElementWithBackgroundImage from "./utils/ElementWithBackgroundImage.tsx";
+import ListElement from "./views/ListElement.tsx";
+import { ecAccountManagement, ecGlobalExpansion, ecTrolley } from "./utils/customIconDefinition.ts";
 
 export default (
   <Body title="Global Expansion">
@@ -57,7 +59,67 @@ export default (
         </figcaption>
         <Picture src="/images/discussion.jpg" alt="" />
       </figure>
-      <WhatWeDo />
+      <ElementWithBackgroundImage
+        tagName="section"
+        src="/images/Ecomma-b2.jpg"
+        class="about-us"
+      >
+        <h3>Our Expertise</h3>
+        <p>
+          From handling logistics and taxes to product translation and
+          localizations, we've got you covered. Sit back and relax with our
+          hassle-free full service solution.
+        </p>
+        <div role="list">
+          <ListElement
+            title="Strategy & Planning"
+            desc="Learn what it takes to be successful on Amazon and in eCommerce. Once you’re up to speed, our experts help you define the best growth strategy for your brands and market."
+          />
+          <ListElement
+            icon={ecGlobalExpansion as IconProp}
+            title="EU Expansion"
+            desc="Don’t miss out on the huge opportunity that is Europe. Split into multiple countries, you’ll have to conquer them one by one but don’t worry; our native speakers will get you ready in no time."
+          />
+          <ListElement
+            icon={ecGlobalExpansion as IconProp}
+            title="US Expansion"
+            desc="The US is Amazon’s home base, which means the largest market to sell in. We’ve helped many brands expand from Europe or Asia to the US; let us help you thrive in this key market."
+          />
+          <ListElement
+            icon={ecGlobalExpansion as IconProp}
+            title="Asia Expansion"
+            desc="The Asian eCommerce market is massive, but with our local teams on hand, we can help you navigate these complex marketplaces and expand on Amazon in India, Singapore and Japan."
+          />
+          <ListElement
+            icon={ecGlobalExpansion as IconProp}
+            title="RoW Expansion"
+            desc="Want to expand in South America, South Africa or Australia? We work closely with trusted local partners that can help you reach and succeed in each corner of the world."
+          />
+          <ListElement
+            icon={ecAccountManagement as IconProp}
+            title="Account Management"
+            desc="We can manage the day-to-day aspects of your eCommerce account. From cataloguing to handling your retail media campaigns and analytics reporting, we’ve got you covered."
+          />
+          <ListElement
+            icon={ecTrolley as IconProp}
+            title="Retail"
+            desc="Use our Seller account and team of experts to import and sell your products in your name. The best part? We’ll take care of taxes and all the management of the account."
+          />
+          <ListElement
+            icon={faBullhorn as IconProp}
+            title="Retail Media"
+            desc="Let us handle your social media, search engine and marketplace advertising. We can also give you access to AMC (Amazon Marketing Cloud) and Amazon Marketing Stream to help you grow faster."
+          />
+          <ListElement
+            icon={faMagnifyingGlass as IconProp}
+            title="Technology & Analytics"
+            desc="Our multilingual team can target your strategy to drive sales in new markets and engage online shoppers worldwide with their knowledge of the nuances of different countries, languages and cultures."
+          />
+        </div>
+        <a href="about:blank" class="cta animatable">
+          Book a Call
+        </a>
+      </ElementWithBackgroundImage>
     </main>
     <section id="how-we-do-it">
       <h2>How We Do It</h2>
@@ -91,7 +153,7 @@ export default (
         launch in all the European marketplaces and the USA.
       </p>
       <figure>
-        <Picture src={"/images/theResults.png"} alt={""} />
+        <Picture src={"/images/case-studies/mycarrypotty.png"} alt={""} />
         <figcaption>
           <h3>Creating content for other countries</h3>
           <p>
