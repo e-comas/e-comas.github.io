36c1383a3a20a426031b99b159d482159d4e0adc
Stéphane du Hamel <stephduh@live.fr>
2023-09-18 11:25:21 +0200

    podcast: new hero

--- src/podcast.tsx
+++ src/podcast.tsx
@@ -7,19 +7,22 @@ import Footer from "./views/Footer.tsx";
 import { EComas, ECommerce } from "./views/eWords.tsx";
 
 import "runtime:./utils/podcast_feeds.ts";
+import Hero from "./views/hero.tsx";
 import "./resources.scss";
 
 export default (
   <Body title="Podcast">
     <Header />
-    <Breadcrumbs />
+    {/* <Breadcrumbs /> */}
+      <Hero>
+        <h1>Podcast</h1>
+        <p>
+          Listen to the <ECommerce /> Made Simple podcast with <EComas /> CEO
+          Jérôme de Guigné and our expert friends to find out how to grow and
+          succeed in eCommerce.
+        </p>
+      </Hero>
     <main id="main-content">
-      <h2>Listen to our podcast</h2>
-      <p>
-        Introducing the <EComas /> podcast, now available on your favourite
-        streaming services! Listen now to find out how to grow on Amazon and
-        other <ECommerce /> platforms.
-      </p>
 
       <section id="podcast">
         <hr />
