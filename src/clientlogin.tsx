aee3755ffaf71e53421394cd190c3df7d1d7e0a7
Stéphane du Hamel <stephduh@live.fr>
2023-09-19 14:57:21 +0200

    Client login: add page (ugly style)

---
+++ src/clientlogin.tsx
@@ -0,0 +1,22 @@
+import { h } from "@aduh95/async-jsx";
+import Body from "./views/Body";
+import Header from "./views/Header";
+import yaml from "./utils/yaml";
+
+
+export default(
+    <Body title="Login page">
+        <Header />
+        <main id="main-content">
+            <a href="https://reports.e-comas.com/seller">Seller</a>
+            <a href="https://reports.e-comas.com/seller">Vendor</a>
+        </main>
+    </Body>
+).then(
+  (e) => (
+    document.body.append(e),
+    yaml`
+layout: none
+`
+  )
+);
