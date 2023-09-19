e53279cd11b942ab0a68198b06e56a32f7121c4f
Stéphane du Hamel <stephduh@live.fr>
2023-09-19 13:12:09 +0200

    index: update partners

--- src/views/Partners.tsx
+++ src/views/Partners.tsx
@@ -23,11 +23,14 @@ export default (props: Props | undefined, children: JSX.Element[]) => (
       <a role="listitem" href="website-creation.html#Shopify">
         <img alt="Shopify" src="/images/partners/shopify.svg" />
       </a>
-      <a role="listitem" href="profitero.html">
-        <img alt="Profitero" src="/images/partners/profitero.svg" />
+      <a role="listitem" href="partners.html#pacvue">
+        <img alt="Profitero" src="/images/partners/pacvue.svg" />
       </a>
-      <a role="listitem" href="reveals.html">
-        <Picture alt="Reveals" src="/images/partners/reveals.png" />
+      <a role="listitem" href="partners.html#channelEngine">
+        <Picture alt="Reveals" src="/images/partners/ChannelEngine.svg" />
+      </a>
+      <a role="listitem" href="partners.html#amazonAds">
+        <Picture alt="Reveals" src="/images/partners/Amazon ADS.svg" />
       </a>
     </div>
   </section>
