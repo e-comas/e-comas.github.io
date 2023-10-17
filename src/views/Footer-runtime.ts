c63a17ca2ef5b3420f0cf497108b2ff37bed5af7
Stéphane du Hamel <stephduh@live.fr>
2023-10-17 16:40:36 +0200

    footer form: check validity

--- src/views/Footer-runtime.ts
+++ src/views/Footer-runtime.ts
@@ -1,11 +1,14 @@
 export default function onSubmit(_token: any) {
-  const pt0 = "go.e-com";
-  const pt1 = "as.com&l&88573";
-  const pt2 = "3&2023-10-10&m95z7";
-  console.log('clicked');
-  const formAction = ("https:&&" + pt0 + pt1 + pt2).replaceAll("&", "/");
-  (document.getElementById("Requests") as HTMLFormElement).action = formAction;
-  (document.getElementById("Requests") as HTMLFormElement)?.submit();
+  if((document.getElementById("Requests") as HTMLFormElement)?.reportValidity()){
+    const pt0 = "go.e-com";
+    const pt1 = "as.com&l&88573";
+    const pt2 = "3&2023-10-10&m95z7";
+    const formAction = ("https:&&" + pt0 + pt1 + pt2).replaceAll("&", "/");
+    (document.getElementById("Requests") as HTMLFormElement).action = formAction;
+    (document.getElementById("Requests") as HTMLFormElement)?.submit();
+  }else{
+    console.log("invalid form content,skip submission");
+  }
 }
 
 document.getElementById("submitRequest")?.addEventListener("click", onSubmit);
