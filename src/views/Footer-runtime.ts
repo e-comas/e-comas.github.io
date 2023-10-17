360e6f5c7209723978280cf26cf6be485ef006a8
Stéphane du Hamel <stephduh@live.fr>
2023-10-17 14:37:49 +0200

    footer: add debug logging

--- src/views/Footer-runtime.ts
+++ src/views/Footer-runtime.ts
@@ -2,7 +2,7 @@ export default function onSubmit(_token: any) {
   const pt0 = "go.e-com";
   const pt1 = "as.com&l&88573";
   const pt2 = "3&2023-10-10&m95z7";
-
+  console.log('clicked');
   const formAction = ("https:&&" + pt0 + pt1 + pt2).replaceAll("&", "/");
   (document.getElementById("Requests") as HTMLFormElement).action = formAction;
   (document.getElementById("Requests") as HTMLFormElement)?.submit();
