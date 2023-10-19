9b0ee7c0f5d1130e2c7209ad76e4112395e84a63
Stéphane du Hamel <stephduh@live.fr>
2023-10-19 14:14:57 +0200

    contact: fix lag

--- src/utils/add-plausible-class-in-iframe.ts
+++ src/utils/add-plausible-class-in-iframe.ts
@@ -1,24 +1,25 @@
 var edited = false;
+function setClass() {
+  // Get a reference to the iframe by its ID
+  const iframe = document.getElementById("myIframe") as HTMLIFrameElement;
+  // Check if the iframe exists and is loaded
+  // Check if the iframe exists
+  if (iframe && iframe.contentWindow) {
+    // Access the content of the iframe
+    const iframeWindow = iframe.contentWindow;
+    const iframeDocument = iframeWindow.document;
 
-while (!edited) {
-  setTimeout(() => {
-    // Get a reference to the iframe by its ID
-    const iframe = document.getElementById("myIframe") as HTMLIFrameElement;
-    // Check if the iframe exists and is loaded
-    // Check if the iframe exists
-    if (iframe && iframe.contentWindow) {
-      // Access the content of the iframe
-      const iframeWindow = iframe.contentWindow;
-      const iframeDocument = iframeWindow.document;
+    // Find the form element within the iframe content
+    const form = iframeDocument.getElementsByTagName("form")[0]; // Replace "myForm" with the actual ID of your form
 
-      // Find the form element within the iframe content
-      const form = iframeDocument.getElementsByTagName("form")[0]; // Replace "myForm" with the actual ID of your form
-
-      // Add a class to the form element
-      if (form) {
-        form.classList.add("plausible-event-name=Form+Signup");
-      }
-      edited = true;
+    // Add a class to the form element
+    if (form) {
+      form.classList.add("plausible-event-name=Form+Signup");
     }
-  }, 200);
+    edited = true;
+  } else {
+    setTimeout(() => setClass(), 200);
+  }
 }
+
+setClass();
