7eefec55cc1fe12a5c1b2305ab9bd8952d0c4a21
Stéphane du Hamel <stephduh@live.fr>
2024-03-04 17:58:22 +0100

    Fix pardot tracking script

--- scripts/prod-build-html.mjs
+++ scripts/prod-build-html.mjs
@@ -41,7 +41,7 @@ function addAnalyticsSnippet(DEALFRONT_ID, ANALYTICS_ID, GTAG_ID) {
 
   const scriptPardot = document.createElement("script");
   scriptPardot.type = "text/javascript";
-  scriptPardot.innerHTML = `piAId="886733";piCId="8017";piHostname="pi.pardot.com";(function(){functionasync_load(){vars=document.createElement("script");s.type="text/javascript";s.src=("https:"==document.location.protocol?"https://pi":"http://cdn")+".pardot.com/pd.js";varc=document.getElementsByTagName("script")[0];c.parentNode.insertBefore(s,c);}if(window.attachEvent){window.attachEvent("onload",async_load);}else{window.addEventListener("load",async_load,false);}})();`
+  scriptPardot.innerHTML = `piAId="886733";piCId="8017";piHostname="pi.pardot.com";(function(){functionasync_load(){var s=document.createElement("script");s.type="text/javascript";s.src=("https:"==document.location.protocol?"https://pi":"http://cdn")+".pardot.com/pd.js";var c=document.getElementsByTagName("script")[0];c.parentNode.insertBefore(s,c);}if(window.attachEvent){window.attachEvent("onload",async_load);}else{window.addEventListener("load",async_load,false);}})();`
   
   document.head.append(
     script0,
