300fae1b40cc698b5e75e767fc3c325edfd6da40
Stéphane du Hamel <stephduh@live.fr>
2024-03-05 19:42:19 +0100

    Prod: remove e-Comas pi script

--- scripts/prod-build-html.mjs
+++ scripts/prod-build-html.mjs
@@ -43,19 +43,13 @@ function addAnalyticsSnippet(DEALFRONT_ID, ANALYTICS_ID, GTAG_ID) {
   const scriptPardot = document.createElement("script");
   scriptPardot.type = "text/javascript";
   scriptPardot.innerHTML = `piAId="${PARDOT_ID.Aid}";piCId="${PARDOT_ID.Cid}";piHostname="${PARDOT_ID.hostname}";(function(){functionasync_load(){var s=document.createElement("script");s.type="text/javascript";s.src=("https:"==document.location.protocol?"https://pi":"http://cdn")+".pardot.com/pd.js";var c=document.getElementsByTagName("script")[0];c.parentNode.insertBefore(s,c);}if(window.attachEvent){window.attachEvent("onload",async_load);}else{window.addEventListener("load",async_load,false);}})();`;
-
-  const scriptEcom = document.createElement("script");
-  scriptEcom.type = "text/javascript";
-  scriptEcom.innerHTML = `piAId="886733";piCId="";piHostname="go.e-comas.com";(function(){functionasync_load(){var s=document.createElement("script");s.type="text/javascript";s.src=("https:"==document.location.protocol?"https://":"http://")+piHostname+"/pd.js";var c=document.getElementsByTagName("script")[0];c.parentNode.insertBefore(s,c);}if(window.attachEvent){window.attachEvent("onload",async_load);}else{window.addEventListener("load",async_load,false);}})();`;
-
   document.head.append(
     script0,
     scriptPlausible,
     scriptDealFront,
     scriptGTM,
     scriptGTag,
-    scriptPardot,
-    scriptEcom
+    scriptPardot
   );
 }
 
