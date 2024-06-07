2f0a9f1294d4e401683c812c99cdb441fce4c9e9
Stéphane du Hamel <stephduh@live.fr>
2024-06-07 17:53:34 +0200

    Blog: add newsletter sticky

--- src/_layouts/blog.tsx
+++ src/_layouts/blog.tsx
@@ -6,21 +6,27 @@ import Breadcrumbs from "../views/Breadcrumbs.tsx";
 import Footer from "../views/Footer.tsx";
 
 import "./blog.scss";
+import Newsletter from "../views/newsletter.tsx";
 
 export default (
   <Body title="{{ page.title }}">
     <Header />
     {/* <Breadcrumbs /> */}
-    <main id="main-content">
-      <h2>{"{{ page.title | escape }}"}</h2>
-      <div class="publishing-info">
-        <time dateTime={"{{ page.date | date: '%Y-%m-%dT%H:%M:%S' }}"}>
-          {'{{ page.date | date: "%a, %b %d, %Y" }}'}
-        </time>
-        {" – Written by {{ page.author | escape }}"}
-      </div>
-      {"{{ content }}"}
-    </main>
+    <div class="blog-page">
+      <main id="main-content">
+        <h2>{"{{ page.title | escape }}"}</h2>
+        <div class="publishing-info">
+          <time dateTime={"{{ page.date | date: '%Y-%m-%dT%H:%M:%S' }}"}>
+            {'{{ page.date | date: "%a, %b %d, %Y" }}'}
+          </time>
+          {" – Written by {{ page.author | escape }}"}
+        </div>
+        {"{{ content }}"}        
+      </main>
+      <aside>
+        <Newsletter />
+      </aside>
+    </div>
     <Footer />
   </Body>
 ).then((e) => document.body.append(e));
