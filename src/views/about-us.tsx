d9fd3229e356d7ccf6c906548e8cd061f1318e5c
Stéphane du Hamel <stephduh@live.fr>
2024-08-12 16:26:15 +0200

    Update about-us picture

--- src/views/about-us.tsx
+++ src/views/about-us.tsx
@@ -12,7 +12,7 @@ import { IconProp } from "@fortawesome/fontawesome-svg-core";
 export default () => (
   <ElementWithBackgroundImage
     tagName="section"
-    src="/images/e-comas-73.jpg"
+    src="/images/e-comas-16-crop.jpg"
     class="about-us"
   >
     <h2>About us</h2>
