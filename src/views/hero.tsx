b66fc472ad4c1e4aa41cac24eb758c45ae4c7214
Stéphane du Hamel <stephdham@gmail.com>
2026-02-24 11:58:12 +0100

    Entity pages

--- src/views/hero.tsx
+++ src/views/hero.tsx
@@ -1,16 +1,25 @@
 import { h } from "@aduh95/async-jsx";
 import "./hero.scss";
 
+export var entityClasses = {
+  "group": "group",
+  "marketplace": "marketplace",
+  "distribution": "distribution",
+  "technology": "technology",
+  "digital Marketing": "digital-marketing"
+};
 interface HeroProps
   extends React.DetailedHTMLProps<
     React.HTMLAttributes<HTMLElement>,
     HTMLElement
   > {
+  entity?: string;
   captionClass?: string;
   imgClass?: string;
 }
 export default (props: HeroProps | undefined, children: any[]) => {
-  const className = "hero " + (props?.class || props?.className || "");
+  const entity = props?.entity || "group";
+  const className = "hero " + (props?.class || props?.className || "") + " " + entity;
   return h(
     "div",
     {
