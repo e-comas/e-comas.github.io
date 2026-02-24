2b0cc1b51004b982d6b0e539d6172610f99c9bc7
Stéphane du Hamel <stephdham@gmail.com>
2026-02-24 11:58:12 +0100

    Entity pages

---
+++ src/marketplace.tsx
@@ -0,0 +1,75 @@
+import { h, Fragment } from "@aduh95/async-jsx";
+
+import yaml from "./utils/yaml.ts";
+
+import Body from "./views/Body.tsx";
+import Header from "./views/Header.tsx";
+import Footer from "./views/Footer.tsx";
+
+import "./global-expansion.scss";
+import Hero, { entityClasses } from "./views/hero.tsx";
+
+import "./utils/open-form-in-modal.scss";
+import "runtime:./utils/open-form-in-modal.ts";
+
+import "./views/animate-in.scss";
+import "runtime:./views/animate-in.ts";
+import { FontAwesomeIcon } from "@aduh95/jsx-fontawesome";
+import {
+  faBullhorn,
+  faDownload,
+  faMagnifyingGlass,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { EComas, ECommerce } from "./views/eWords.tsx";
+
+
+export default (
+  <Body title="Global Expansion">
+    <Header />
+    <Hero class="animate-in long-page" entity={entityClasses.marketplace}>
+      <h1 class="title">Marketplace</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </p>
+      <a href="./contact.html" class="cta no-arrow">
+        Book a call
+        <FontAwesomeIcon icon={faPhone as IconProp} />
+      </a>
+    </Hero>
+    <main id="main-content">
+      
+    </main>
+    <section id="blog" class="blog">
+      <h2>Our blog</h2>
+      <p>
+        Read our latest blogs for helpful <ECommerce /> tips and what's new in
+        the Amazon world relating to expanding your brand internationally.
+      </p>
+      <div role="list">
+        {"{% for post in site.tags.Expansion | slice: 0, 3 %}"}
+        <article>
+          <a href="{{ post.url }}">
+            <img
+              src="{{ post.thumbnail }}"
+              alt="{{ post.thumbnail_alt }}"
+              loading="lazy"
+            />
+          </a>
+          <h3>
+            <a href="{{ post.url }}">{"{{ post.title | escape }}"}</a>
+          </h3>
+          <p>{"{{ post.excerpt | escape }}"}</p>
+          <a href="{{ post.url }}" class="cta">
+            Read More
+          </a>
+        </article>
+        {"{% endfor %}"}
+      </div>
+    </section>
+    <Footer />
+  </Body>
+).then((e) => (document.body.append(e), yaml`layout: none`));
